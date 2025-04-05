c59029e16265644dc66df4e3930d5e1e59a336bc
Louis Aurea <louis@nextstation.cl>
2025-04-05 13:08:55 -0400

    no env

--- Backend/app/api/api.py
+++ Backend/app/api/api.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from app.api.endpoints.debug import router as router_debug
 from app.api.endpoints.post import router as router_post
+from app.controllers.authentication import jwtBearer
 
 api_router = APIRouter()
 
@@ -8,4 +9,5 @@ api_router.include_router(router_post, tags=["post"],
     responses={404: {"description": "Not found"}})
 
 api_router.include_router(router_debug, tags=["Debug"],
-    responses={404: {"description": "Not found"}})
+    responses={404: {"description": "Not found"}},
+     dependencies=[Depends(jwtBearer())])
